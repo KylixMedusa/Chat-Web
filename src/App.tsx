9e7cf203c25adba63ae74bfb8adfe2b1717eb384
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-10-11 22:09:42 +0530

    Removed default styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainChat from './Containers/Main-Chat/Main-Chat';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <MainChat></MainChat>
+    </React.Fragment>
   );
 }
 
