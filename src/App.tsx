0fdec727125f08bcb4051f6cf7c6b3ba56c38b42
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-10-16 21:00:40 +0530

    Added some containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import AllChats from './Components/All-Chats/All-Chats';
 import Navbar from './Components/Navbar/Navbar';
+import ChannelsSection from './Containers/ChannelsSection/ChannelsSection';
 import MainChat from './Containers/Main-Chat/Main-Chat';
+import SideSection from './Containers/SideSection/SideSection';
 
 function App() {
   return (
     <div className="flex-container">
       <Navbar></Navbar>
-      <AllChats></AllChats>
+      <ChannelsSection></ChannelsSection>
       <MainChat></MainChat>
+      <SideSection></SideSection>
     </div>
   );
 }
