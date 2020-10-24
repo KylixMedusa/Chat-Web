679832f526834422c41f3c462d3f94ba3efc3050
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-10-24 22:23:32 +0530

    Added unicode to decode emojis

--- src/Containers/Main-Chat/Main-Chat.tsx
+++ src/Containers/Main-Chat/Main-Chat.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import Chat from "../../Sub-Components/Chat/Chat";
 import "./Main-Chat.scss";
 import Picker from "emoji-picker-react";
+import * as emojiUnicode from 'emoji-unicode'
 import ContentEditable from "../../Components/ContentEditable";
 
 const MainChat: React.FC = () => {
@@ -24,6 +25,9 @@ const MainChat: React.FC = () => {
 
   function focus() {
     // setPos(cursor);
+    if (inputElem.current) {
+      inputElem.current.focus();
+    }
   }
 
   function setPos(pos: number) {
@@ -144,6 +148,25 @@ const MainChat: React.FC = () => {
       setPickerClass("close");
     } else {
       setPickerClass("open");
+    }
+  }
+
+  function checkEmoji(){
+    let temp = inputElem.current?.innerText.charAt(inputElem.current.innerText.length - 1);
+    if(temp && temp!=''){
+      if(isEmoji(temp)){
+        console.log(emojiUnicode(temp))
+      }
+    }
+  }
+  function isEmoji(str:any) {
+    var ranges = [
+        '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])' // U+1F680 to U+1F6FF
+    ];
+    if (str.match(ranges.join('|'))) {
+        return true;
+    } else {
+        return false;
     }
   }
 
@@ -273,7 +296,7 @@ const MainChat: React.FC = () => {
               id="input"
               ref={inputElem}
               onSelectCapture={(e) => setCursorPos(e)}
-              onInput={setVisiblityHandler}
+              onInput={()=>{setVisiblityHandler(); checkEmoji()}}
             ></div>
           </div>
         </div>
