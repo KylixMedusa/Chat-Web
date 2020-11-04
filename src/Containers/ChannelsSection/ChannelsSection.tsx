feaf7f12e20abfe057359cd49f22a8f97286299d
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-11-04 22:06:34 +0530

    Added settings component

--- src/Containers/ChannelsSection/ChannelsSection.tsx
+++ src/Containers/ChannelsSection/ChannelsSection.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './ChannelsSection.scss'
 import AllChats from '../../Components/All-Chats/All-Chats';
+import Settings from '../../Components/Settings/Settings';
 
 const ChannelsSection:React.FC = ()=>{
     return(
         <div className="channels-section">
-            <AllChats></AllChats>
+            <Settings></Settings>
+            {/* <AllChats></AllChats> */}
         </div>
     );
 }
