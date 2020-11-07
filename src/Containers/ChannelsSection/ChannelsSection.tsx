bcef8a8d6be74e73f2f35385d417541b29823036
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-11-07 21:27:09 +0530

    Changed Channels section to Contacts section

--- src/Containers/ChannelsSection/ChannelsSection.tsx
+++ src/Containers/ChannelsSection/ChannelsSection.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './ChannelsSection.scss'
 import AllChats from '../../Components/All-Chats/All-Chats';
 import Settings from '../../Components/Settings/Settings';
+import Contacts from '../../Components/Contacts/Contacts';
 
 const ChannelsSection:React.FC = ()=>{
     return(
         <div className="channels-section">
-            <Settings></Settings>
+            {/* <Settings></Settings> */}
             {/* <AllChats></AllChats> */}
+            <Contacts></Contacts>
         </div>
     );
 }
