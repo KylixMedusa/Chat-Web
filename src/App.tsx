2c7f1906430ee565d22dfcd290fb717669a632b6
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-05 20:48:54 +0530

    Added function and observable to handle theme

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,23 @@ import { observable } from "mobx";
 import { observer } from "mobx-react-lite";
 
 export const channelSectionHandler = observable.box("Chats");
+export const dataTheme = observable.box("light");
+
+export const checkSystemTheme = ()=>{
+  const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+  if(userPrefersDark){
+    dataTheme.set("dark");
+  }
+  else{
+    dataTheme.set("light");
+  }
+  
+}
 
 const App: React.FC = () => {
   return (
-    <div className="flex-container">
+    <div className="flex-container" data-theme={dataTheme.get()}>
       <Navbar></Navbar>
       <ChannelsSection></ChannelsSection>
       <div className="wrapper-side">
