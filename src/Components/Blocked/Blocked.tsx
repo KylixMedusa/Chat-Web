54ca127690b4d653e41e42d933bb61ddc310448e
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-08 21:11:07 +0530

    Added blocked component with basic styling

---
+++ src/Components/Blocked/Blocked.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./Blocked.scss";
+
+import { observer } from "mobx-react-lite";
+
+import { channelSectionHandler } from "../../App";
+
+const Blocked: React.FC = () => {
+  return (
+    <div className="blocked-section">
+      <div className="top-bar">
+        <div
+          role="button"
+          className="icon"
+          onClick={() => {
+            channelSectionHandler.set("Settings");
+          }}
+        >
+          <i>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+            >
+              <path
+                fill="currentColor"
+                d="M12 4l1.4 1.4L7.8 11H20v2H7.8l5.6 5.6L12 20l-8-8 8-8z"
+              ></path>
+            </svg>
+          </i>
+        </div>
+        <h2
+          className="channel-title"
+          style={{ marginBottom: "0px", paddingLeft: "0px" }}
+        >
+          Blocked
+        </h2>
+      </div>
+      <div style={{ overflowY: "auto", height: "100%" }}>
+        
+      </div>
+    </div>
+  );
+};
+
+export default observer(Blocked);
