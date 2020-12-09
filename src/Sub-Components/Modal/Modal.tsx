7cefb127408eea4ccb28441e8ed0fe6e184c2efe
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-09 22:26:13 +0530

    Added style props

--- src/Sub-Components/Modal/Modal.tsx
+++ src/Sub-Components/Modal/Modal.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import './Modal.scss';
 
 
 type Props = {
     class:string;
     toggle:any;
+    style?:CSSProperties;
 }
 
 
 const Modal:React.FC<Props> = (props)=>{
     return (
         <div className={`modal ${props.class}`}>
-            <div className="modal-wrapper">
+            <div className="modal-wrapper" style={props.style?props.style:{}}>
                 {props.children}
             </div>
             <div className="modal-overlay" onClick={props.toggle}></div>
