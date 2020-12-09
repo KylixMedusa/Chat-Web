dcf9b9f4aff486fbe56c7e4b2c65e1ccbbbc2705
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-09 22:25:51 +0530

    Added Modal, Updated component

--- src/Components/Blocked/Blocked.tsx
+++ src/Components/Blocked/Blocked.tsx
@@ -4,8 +4,17 @@ import "./Blocked.scss";
 import { observer } from "mobx-react-lite";
 
 import { channelSectionHandler } from "../../App";
+import Modal from "../../Sub-Components/Modal/Modal";
 
 const Blocked: React.FC = () => {
+  const [modalClass, setModalClass] = useState("");
+  const modalToggleHandler = () => {
+    if (modalClass === `open`) {
+      setModalClass(`close`);
+    } else {
+      setModalClass(`open`);
+    }
+  };
   return (
     <div className="blocked-section">
       <div className="top-bar">
@@ -38,8 +47,45 @@ const Blocked: React.FC = () => {
         </h2>
       </div>
       <div style={{ overflowY: "auto", height: "100%" }}>
-        
+        <div className="card">
+          <div className="card-image">
+            <img src="https://cliko.in/assets/team/aayush.jpg" alt="" />
+          </div>
+          <div className="card-contents">
+            <h3>Aayush Agarwal</h3>
+            <p>
+              Hello, Welcome to the first test. This is a first version of Chat
+              Application
+            </p>
+          </div>
+          <div className="button">
+            <i onClick = {modalToggleHandler}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+              >
+                <path
+                  fill="var(--icon-color-3)"
+                  d="M17.25 7.8L16.2 6.75l-4.2 4.2-4.2-4.2L6.75 7.8l4.2 4.2-4.2 4.2 1.05 1.05 4.2-4.2 4.2 4.2 1.05-1.05-4.2-4.2 4.2-4.2z"
+                ></path>
+              </svg>
+            </i>
+          </div>
+        </div>
       </div>
+      <Modal class={modalClass} toggle={modalToggleHandler} style={{height:'150px'}}>
+        <div>
+          <div className="modal-title">
+            <p>Unblock </p>
+          </div>
+        </div>
+        <div className="modal-buttons">
+          <button className="cancel" onClick={modalToggleHandler}>Cancel</button>
+          <button className="submit" onClick={()=>{modalToggleHandler()}}>OK</button>
+        </div>
+      </Modal>
     </div>
   );
 };
