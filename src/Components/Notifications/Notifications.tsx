ff0c859768996712a1eb2839394f5a32803ade3c
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-09 21:15:28 +0530

    Added states, functions and handling for fields

--- src/Components/Notifications/Notifications.tsx
+++ src/Components/Notifications/Notifications.tsx
@@ -4,8 +4,17 @@ import "./Notifications.scss";
 import { observer } from "mobx-react-lite";
 
 import { channelSectionHandler } from "../../App";
+import { updateStatement } from "typescript";
 
 const Notifications: React.FC = () => {
+  const [checkedState,setCheckedState] = useState([false,false,false,false]);
+
+  function updateState(pos:number){
+    let items = [...checkedState];
+    items[pos] = !items[pos];
+    setCheckedState([...items]);
+  }
+
   return (
     <div className="notifications-section">
       <div className="top-bar">
@@ -41,21 +50,85 @@ const Notifications: React.FC = () => {
         <div className="notifications-wrapper">
           <div className="checkbox-card">
             <div className="checkbox-wrapper">
-              <input className="input-checkbox" type="checkbox" id="msg-previews"/>
+              <input
+                className="input-checkbox"
+                type="checkbox"
+                id="sounds"
+                onChange={()=>updateState(0)}
+                checked={checkedState[0]}
+              />
               <div className="custom-checkbox-wrapper">
                 <div className="custom-checkbox">
-                  <div className="tick-mark">
-                  </div>
+                  <div className="tick-mark"></div>
                 </div>
               </div>
             </div>
-            <label className="checkbox-label" htmlFor="msg-previews">Show Previews
-              <div className="checkbox-label-secondary">Display message text in desktop alerts</div>
+            <label className="checkbox-label" htmlFor="sounds">
+              Sounds
+            </label>
+          </div>
+          <div className="checkbox-card">
+            <div className="checkbox-wrapper">
+              <input
+                className="input-checkbox"
+                type="checkbox"
+                id="desktop-alerts"
+                onChange={()=>updateState(1)}
+                checked={checkedState[1]}
+              />
+              <div className="custom-checkbox-wrapper">
+                <div className="custom-checkbox">
+                  <div className="tick-mark"></div>
+                </div>
+              </div>
+            </div>
+            <label className="checkbox-label" htmlFor="desktop-alerts">
+              Desktop Alerts
+            </label>
+          </div>
+          <div className="checkbox-card">
+            <div className="checkbox-wrapper">
+              <input
+                className="input-checkbox"
+                type="checkbox"
+                id="msg-previews"
+                onChange={()=>updateState(2)}
+                checked={checkedState[2]}
+              />
+              <div className="custom-checkbox-wrapper">
+                <div className="custom-checkbox">
+                  <div className="tick-mark"></div>
+                </div>
+              </div>
+            </div>
+            <label className="checkbox-label" htmlFor="msg-previews">
+              Show Previews
+              <div className="checkbox-label-secondary">
+                Display message text in desktop alerts
+              </div>
+            </label>
+          </div>
+          <div className="checkbox-card">
+            <div className="checkbox-wrapper">
+              <input
+                className="input-checkbox"
+                type="checkbox"
+                id="destop-notifications"
+                onChange={()=>updateState(3)}
+                checked={checkedState[3]}
+              />
+              <div className="custom-checkbox-wrapper">
+                <div className="custom-checkbox">
+                  <div className="tick-mark"></div>
+                </div>
+              </div>
+            </div>
+            <label className="checkbox-label" htmlFor="destop-notifications">
+              Turn off Destop Notifications
             </label>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
