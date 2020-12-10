3c4ee9bbf711726e66e4283b00c2c462dc38594e
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-10 17:04:15 +0530

    Added tsx, updated UI

--- src/Components/Blocked/Blocked.tsx
+++ src/Components/Blocked/Blocked.tsx
@@ -47,43 +47,89 @@ const Blocked: React.FC = () => {
         </h2>
       </div>
       <div style={{ overflowY: "auto", height: "100%" }}>
-        <div className="card">
-          <div className="card-image">
-            <img src="https://cliko.in/assets/team/aayush.jpg" alt="" />
-          </div>
-          <div className="card-contents">
-            <h3>Aayush Agarwal</h3>
-            <p>
-              Hello, Welcome to the first test. This is a first version of Chat
-              Application
-            </p>
-          </div>
-          <div className="button">
-            <i onClick = {modalToggleHandler}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-              >
-                <path
-                  fill="var(--icon-color-3)"
-                  d="M17.25 7.8L16.2 6.75l-4.2 4.2-4.2-4.2L6.75 7.8l4.2 4.2-4.2 4.2 1.05 1.05 4.2-4.2 4.2 4.2 1.05-1.05-4.2-4.2 4.2-4.2z"
-                ></path>
-              </svg>
-            </i>
-          </div>
-        </div>
+        <ul>
+          <li>
+            <div className="card">
+              <div className="card-image">
+                <img src="https://cliko.in/assets/team/aayush.jpg" alt="" />
+              </div>
+              <div className="card-contents">
+                <h3>Aayush Agarwal</h3>
+                <p>
+                  Hello, Welcome to the first test. This is a first version of
+                  Chat Application
+                </p>
+              </div>
+              <div className="button">
+                <i onClick={modalToggleHandler}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path
+                      fill="var(--icon-color-3)"
+                      d="M17.25 7.8L16.2 6.75l-4.2 4.2-4.2-4.2L6.75 7.8l4.2 4.2-4.2 4.2 1.05 1.05 4.2-4.2 4.2 4.2 1.05-1.05-4.2-4.2 4.2-4.2z"
+                    ></path>
+                  </svg>
+                </i>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="card">
+              <div className="card-image">
+                <img src="https://cliko.in/assets/team/aayush.jpg" alt="" />
+              </div>
+              <div className="card-contents">
+                <h3>Aayush Agarwal</h3>
+                <p>
+                  Hello, Welcome to the first test. This is a first version of
+                  Chat Application
+                </p>
+              </div>
+              <div className="button">
+                <i onClick={modalToggleHandler}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path
+                      fill="var(--icon-color-3)"
+                      d="M17.25 7.8L16.2 6.75l-4.2 4.2-4.2-4.2L6.75 7.8l4.2 4.2-4.2 4.2 1.05 1.05 4.2-4.2 4.2 4.2 1.05-1.05-4.2-4.2 4.2-4.2z"
+                    ></path>
+                  </svg>
+                </i>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
-      <Modal class={modalClass} toggle={modalToggleHandler} style={{height:'150px'}}>
+      <Modal
+        class={modalClass}
+        toggle={modalToggleHandler}
+        style={{ height: "150px" }}
+      >
         <div>
           <div className="modal-title">
             <p>Unblock </p>
           </div>
         </div>
         <div className="modal-buttons">
-          <button className="cancel" onClick={modalToggleHandler}>Cancel</button>
-          <button className="submit" onClick={()=>{modalToggleHandler()}}>OK</button>
+          <button className="cancel" onClick={modalToggleHandler}>
+            Cancel
+          </button>
+          <button
+            className="submit"
+            onClick={() => {
+              modalToggleHandler();
+            }}
+          >
+            OK
+          </button>
         </div>
       </Modal>
     </div>
