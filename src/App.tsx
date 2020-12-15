b3ce508477078d8f815a13a9dc2f335d7879cd83
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-15 22:10:38 +0530

    Updated function for pattern

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import MainChat, { chatBackground } from "./Containers/Main-Chat/Main-Chat";
 import SideSection from "./Containers/SideSection/SideSection";
 import { observable } from "mobx";
 import { observer } from "mobx-react-lite";
-import { handleWallpaper, setInitWallpaper } from "./Components/Wallpaper/Wallpaper";
+import { handleWallpaper, patternView, setInitWallpaper } from "./Components/Wallpaper/Wallpaper";
 
 export const channelSectionHandler = observable.box("Chats");
 export const profileData = observable.box({
@@ -45,6 +45,18 @@ function checkTheme() {
   }
   else{
     handleWallpaper(dataTheme.get());
+  }
+  let pattern = localStorage.getItem("pattern");
+  if(pattern){
+    if(pattern === "false"){
+      patternView.set(false);
+    }
+    else{
+      patternView.set(true);
+    }
+  }
+  else{
+    localStorage.setItem("pattern", "true");
   }
 }
 export const dataTheme = observable.box("light");
