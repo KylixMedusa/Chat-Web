d885c0c8adc0bed338a4b2fd8617e6b3cf30a2e0
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-15 22:11:35 +0530

    Added pattern selector checkbox

--- src/Components/Wallpaper/Wallpaper.tsx
+++ src/Components/Wallpaper/Wallpaper.tsx
@@ -7,68 +7,69 @@ import {
   chatBackground,
   selectedChatBackground,
 } from "../../Containers/Main-Chat/Main-Chat";
-import { observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
+import { observable } from "mobx";
 
 const colorsLight = [
-  {color:"var(--bg-color-4)", opacity:"0.06", pattern:"dark"},
-  {color:"rgb(187, 228, 229)", opacity:"0.42", pattern:"light"},
-  {color:"rgb(174, 216, 199)", opacity:"0.3", pattern:"light"},
-  {color:"rgb(122, 203, 165)", opacity:"0.26", pattern:"light"},
-  {color:"rgb(203, 218, 236)", opacity:"0.42", pattern:"light"},
-  {color:"rgb(102, 210, 213)", opacity:"0.26", pattern:"light"},
-  {color:"rgb(99, 189, 207)", opacity:"0.26", pattern:"light"},
-  {color:"rgb(214, 208, 240)", opacity:"0.36", pattern:"light"},
-  {color:"rgb(206, 206, 206)", opacity:"0.36", pattern:"light"},
-  {color:"rgb(209, 218, 190)", opacity:"0.36", pattern:"light"},
-  {color:"rgb(230, 225, 177)", opacity:"0.42", pattern:"light"},
-  {color:"rgb(254, 239, 169)", opacity:"0.06", pattern:"dark"},
-  {color:"rgb(254, 210, 151)", opacity:"0.42", pattern:"light"},
-  {color:"rgb(253, 154, 155)", opacity:"0.26", pattern:"light"},
-  {color:"rgb(253, 103, 105)", opacity:"0.26", pattern:"light"},
-  {color:"rgb(251, 70, 104)", opacity:"0.26", pattern:"light"},
-  {color:"rgb(146, 32, 64)", opacity:"0.17", pattern:"light"},
-  {color:"rgb(220, 110, 79)", opacity:"0.2", pattern:"light"},
-  {color:"rgb(10, 77, 82)", opacity:"0.13", pattern:"light"},
-  {color:"rgb(81, 126, 126)", opacity:"0.13", pattern:"light"},
-  {color:"rgb(49, 144, 187)", opacity:"0.13", pattern:"light"},
-  {color:"rgb(53, 85, 138)", opacity:"0.1", pattern:"light"},
-  {color:"rgb(85, 98, 111)", opacity:"0.1", pattern:"light"},
-  {color:"rgb(29, 35, 38)", opacity:"0.1", pattern:"light"},
-  {color:"rgb(48, 30, 52)", opacity:"0.1", pattern:"light"},
-  {color:"rgb(236, 240, 241)", opacity:"0.06", pattern:"dark"},
-  {color:"rgb(255, 254, 162)", opacity:"0.06", pattern:"dark"},
-  {color:"rgb(231, 232, 210)", opacity:"0.06", pattern:"dark"},
+  { color: "var(--bg-color-4)", opacity: "0.06", pattern: "dark" },
+  { color: "rgb(187, 228, 229)", opacity: "0.42", pattern: "light" },
+  { color: "rgb(174, 216, 199)", opacity: "0.3", pattern: "light" },
+  { color: "rgb(122, 203, 165)", opacity: "0.26", pattern: "light" },
+  { color: "rgb(203, 218, 236)", opacity: "0.42", pattern: "light" },
+  { color: "rgb(102, 210, 213)", opacity: "0.26", pattern: "light" },
+  { color: "rgb(99, 189, 207)", opacity: "0.26", pattern: "light" },
+  { color: "rgb(214, 208, 240)", opacity: "0.36", pattern: "light" },
+  { color: "rgb(206, 206, 206)", opacity: "0.36", pattern: "light" },
+  { color: "rgb(209, 218, 190)", opacity: "0.36", pattern: "light" },
+  { color: "rgb(230, 225, 177)", opacity: "0.42", pattern: "light" },
+  { color: "rgb(254, 239, 169)", opacity: "0.06", pattern: "dark" },
+  { color: "rgb(254, 210, 151)", opacity: "0.42", pattern: "light" },
+  { color: "rgb(253, 154, 155)", opacity: "0.26", pattern: "light" },
+  { color: "rgb(253, 103, 105)", opacity: "0.26", pattern: "light" },
+  { color: "rgb(251, 70, 104)", opacity: "0.26", pattern: "light" },
+  { color: "rgb(146, 32, 64)", opacity: "0.17", pattern: "light" },
+  { color: "rgb(220, 110, 79)", opacity: "0.2", pattern: "light" },
+  { color: "rgb(10, 77, 82)", opacity: "0.13", pattern: "light" },
+  { color: "rgb(81, 126, 126)", opacity: "0.13", pattern: "light" },
+  { color: "rgb(49, 144, 187)", opacity: "0.13", pattern: "light" },
+  { color: "rgb(53, 85, 138)", opacity: "0.1", pattern: "light" },
+  { color: "rgb(85, 98, 111)", opacity: "0.1", pattern: "light" },
+  { color: "rgb(29, 35, 38)", opacity: "0.1", pattern: "light" },
+  { color: "rgb(48, 30, 52)", opacity: "0.1", pattern: "light" },
+  { color: "rgb(236, 240, 241)", opacity: "0.06", pattern: "dark" },
+  { color: "rgb(255, 254, 162)", opacity: "0.06", pattern: "dark" },
+  { color: "rgb(231, 232, 210)", opacity: "0.06", pattern: "dark" },
 ];
 
 const colorsDark = [
-  {color:"var(--bg-color-4)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(15, 36, 36)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(18, 38, 31)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(17, 36, 28)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(17, 30, 39)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(15, 34, 36)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(14, 33, 37)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(31, 29, 37)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(33, 33, 33)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(31, 33, 28)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(35, 35, 27)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(38, 36, 25)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(38, 31, 23)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(38, 23, 23)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(38, 15, 16)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(38, 10, 16)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(25, 5, 11)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(33, 16, 12)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(15, 12, 12)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(16, 25, 25)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(10, 29, 37)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(13, 21, 35)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(13, 15, 17)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(10, 12, 13)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(17, 11, 18)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(30, 31, 31)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(38, 38, 24)",opacity:"0.06",pattern:"light"},
-  {color:"rgb(35, 35, 31)",opacity:"0.06",pattern:"light"},
+  { color: "var(--bg-color-4)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(15, 36, 36)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(18, 38, 31)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(17, 36, 28)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(17, 30, 39)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(15, 34, 36)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(14, 33, 37)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(31, 29, 37)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(33, 33, 33)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(31, 33, 28)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(35, 35, 27)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(38, 36, 25)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(38, 31, 23)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(38, 23, 23)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(38, 15, 16)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(38, 10, 16)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(25, 5, 11)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(33, 16, 12)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(15, 12, 12)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(16, 25, 25)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(10, 29, 37)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(13, 21, 35)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(13, 15, 17)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(10, 12, 13)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(17, 11, 18)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(30, 31, 31)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(38, 38, 24)", opacity: "0.06", pattern: "light" },
+  { color: "rgb(35, 35, 31)", opacity: "0.06", pattern: "light" },
 ];
 
 var colors: any[] = [...colorsLight];
@@ -83,7 +84,7 @@ export function handleWallpaper(theme: string) {
   }
 }
 
-export function setInitWallpaper(theme:string, color:string){
+export function setInitWallpaper(theme: string, color: string) {
   if (theme === "dark") {
     colors = [...colorsDark];
     setColorDarkColor(color);
@@ -93,30 +94,34 @@ export function setInitWallpaper(theme:string, color:string){
   }
 }
 
-function setColorDarkColor(color:string) {
+function setColorDarkColor(color: string) {
   if (color != "var(--bg-color-4)") {
     let index = colorsDark.findIndex((set) => {
       return set.color === color;
     });
     chatBackground.set(colorsDark[index]);
     selectedChatBackground.set(colorsDark[index]);
-  }
-  else{
-    chatBackground.set({...colorsDark[0],color:"var(--bg-color-4)"});
-    selectedChatBackground.set({...colorsDark[0],color:"var(--bg-color-4)"});
+  } else {
+    chatBackground.set({ ...colorsDark[0], color: "var(--bg-color-4)" });
+    selectedChatBackground.set({
+      ...colorsDark[0],
+      color: "var(--bg-color-4)",
+    });
   }
 }
-function setColorLightColor(color:string) {
+function setColorLightColor(color: string) {
   if (color != "var(--bg-color-4)") {
     let index = colorsLight.findIndex((set) => {
       return set.color === color;
     });
     chatBackground.set(colorsLight[index]);
     selectedChatBackground.set(colorsLight[index]);
-  }
-  else{
-    chatBackground.set({...colorsLight[0],color:"var(--bg-color-4)"});
-    selectedChatBackground.set({...colorsLight[0],color:"var(--bg-color-4)"});
+  } else {
+    chatBackground.set({ ...colorsLight[0], color: "var(--bg-color-4)" });
+    selectedChatBackground.set({
+      ...colorsLight[0],
+      color: "var(--bg-color-4)",
+    });
   }
 }
 
@@ -125,14 +130,16 @@ function setColorDark() {
     let index = colorsLight.findIndex((set) => {
       return set.color === selectedChatBackground.get().color;
     });
-    localStorage.setItem("color",colorsDark[index].color);
+    localStorage.setItem("color", colorsDark[index].color);
     chatBackground.set(colorsDark[index]);
     selectedChatBackground.set(colorsDark[index]);
-  }
-  else{
-    localStorage.setItem("color","var(--bg-color-4)");
-    chatBackground.set({...colorsDark[0],color:"var(--bg-color-4)"});
-    selectedChatBackground.set({...colorsDark[0],color:"var(--bg-color-4)"});
+  } else {
+    localStorage.setItem("color", "var(--bg-color-4)");
+    chatBackground.set({ ...colorsDark[0], color: "var(--bg-color-4)" });
+    selectedChatBackground.set({
+      ...colorsDark[0],
+      color: "var(--bg-color-4)",
+    });
   }
 }
 function setColorLight() {
@@ -140,22 +147,30 @@ function setColorLight() {
     let index = colorsDark.findIndex((set) => {
       return set.color === selectedChatBackground.get().color;
     });
-    localStorage.setItem("color",colorsLight[index].color);
+    localStorage.setItem("color", colorsLight[index].color);
     chatBackground.set(colorsLight[index]);
     selectedChatBackground.set(colorsLight[index]);
-  }
-  else{
-    localStorage.setItem("color","var(--bg-color-4)");
-    chatBackground.set({...colorsLight[0],color:"var(--bg-color-4)"});
-    selectedChatBackground.set({...colorsLight[0],color:"var(--bg-color-4)"});
+  } else {
+    localStorage.setItem("color", "var(--bg-color-4)");
+    chatBackground.set({ ...colorsLight[0], color: "var(--bg-color-4)" });
+    selectedChatBackground.set({
+      ...colorsLight[0],
+      color: "var(--bg-color-4)",
+    });
   }
 }
 
+export const patternView = observable.box(true);
+
 const Wallpaper: React.FC = () => {
-  
-  useEffect(()=>{
+  useEffect(() => {
     generateColors();
-  },[colors])
+  }, [colors]);
+
+  function updatePatternState() {
+    localStorage.setItem("pattern", String(!patternView.get()));
+    patternView.set(!patternView.get());
+  }
 
   function generateColors() {
     let colorElems = [];
@@ -174,7 +189,7 @@ const Wallpaper: React.FC = () => {
                 chatBackground.set(selectedChatBackground.get());
               }}
               onClick={() => {
-                localStorage.setItem("color",color);
+                localStorage.setItem("color", color);
                 selectedChatBackground.set(colors[0]);
               }}
               key="default"
@@ -194,7 +209,7 @@ const Wallpaper: React.FC = () => {
                 chatBackground.set(selectedChatBackground.get());
               }}
               onClick={() => {
-                localStorage.setItem("color",color);
+                localStorage.setItem("color", color);
                 selectedChatBackground.set(colors[0]);
               }}
               key="default"
@@ -215,7 +230,7 @@ const Wallpaper: React.FC = () => {
                 chatBackground.set(selectedChatBackground.get());
               }}
               onClick={() => {
-                localStorage.setItem("color",color);
+                localStorage.setItem("color", color);
                 selectedChatBackground.set(colors[index]);
               }}
               style={{ backgroundColor: color }}
@@ -234,7 +249,7 @@ const Wallpaper: React.FC = () => {
               }}
               onClick={() => {
                 console.log(color);
-                localStorage.setItem("color",color);
+                localStorage.setItem("color", color);
                 selectedChatBackground.set(colors[index]);
               }}
               style={{ backgroundColor: color }}
@@ -278,7 +293,29 @@ const Wallpaper: React.FC = () => {
           Chat Wallpaper
         </h2>
       </div>
-      <div style={{ overflowY: "auto", height: "100%" }}>
+      <div style={{ overflowY: "auto", display:"flex", alignItems:"center", flexDirection:"column",height: "100%", marginTop:"30px" }}>
+        <div className="checkbox-card">
+          <div className="checkbox-wrapper">
+            <input
+              className="input-checkbox"
+              type="checkbox"
+              id="desktop-alerts"
+              onChange={() => updatePatternState()}
+              checked={patternView.get()}
+            />
+            <div
+              className="custom-checkbox-wrapper"
+              onClick={() => updatePatternState()}
+            >
+              <div className="custom-checkbox">
+                <div className="tick-mark"></div>
+              </div>
+            </div>
+          </div>
+          <label className="checkbox-label" htmlFor="desktop-alerts">
+            Add doodles
+          </label>
+        </div>
         <div className="colors-holder">{generateColors()}</div>
       </div>
     </div>
