898d7feda4998ffe1b28109bd11b2100428f8652
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-18 20:01:24 +0530

    Added Context Provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ContextProvider from './ContextProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
