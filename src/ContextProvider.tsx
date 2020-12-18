49354083cd6318e0916ebc1c30f8d322e3a88824
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-18 20:00:58 +0530

    Added profile and theme context provider

--- src/ContextProvider.tsx
+++ src/ContextProvider.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import * as Store from "./store";
+import ProfileStore from "./store/ProfileStore";
 import SnackbarStore from "./store/SnackbarStore";
+import ThemeStore from "./store/ThemeStore";
 
 const ContextProvider: React.FC<any> = (props) => {
   const snackbarStore = new SnackbarStore();
+  const profileStore = new ProfileStore();
+  const themeStore = new ThemeStore();
 
   return (
     <Store.SnackbarContext.Provider value={snackbarStore}>
-      {props.children}
+      <Store.ProfileContext.Provider value={profileStore}>
+        <Store.ThemeContext.Provider value={themeStore}>
+        {props.children}
+        </Store.ThemeContext.Provider>
+      </Store.ProfileContext.Provider>
     </Store.SnackbarContext.Provider>
   );
 };
