5c13f23556ab1fb6cf9ef63ee8aa0a8920c80eaa
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-18 20:00:38 +0530

    Removed theme functions and added context

--- src/App.tsx
+++ src/App.tsx
@@ -6,82 +6,28 @@ import MainChat from "./Containers/Main-Chat/Main-Chat";
 import SideSection from "./Containers/SideSection/SideSection";
 import { observable } from "mobx";
 import { observer } from "mobx-react-lite";
-import {
-  handleWallpaper,
-  patternView,
-  setInitWallpaper,
-} from "./Components/Wallpaper/Wallpaper";
-import ContextProvider from "./ContextProvider";
 import Snackbar from "./Components/Snackbars/Snackbar";
+import { ThemeContext } from "./store";
+import { useStore } from "./store/hooks";
 
 export const channelSectionHandler = observable.box("Chats");
-export const profileData = observable.box({
-  avatar: "https://cliko.in/assets/team/aayush.jpg",
-  name: "Aayush Agarwal",
-  status: "Can't talk chat only",
-  phone: "+91 9679883985",
-});
-
-export const checkSystemTheme = () => {
-  const userPrefersDark =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-  if (userPrefersDark) {
-    dataTheme.set("dark");
-  } else {
-    dataTheme.set("light");
-  }
-};
-
-function checkTheme() {
-  let theme = localStorage.getItem("theme");
-  if (theme) {
-    selectedTheme.set(theme);
-    dataTheme.set(theme);
-    if (theme === "system default") checkSystemTheme();
-  } else {
-    dataTheme.set("light");
-    selectedTheme.set("light");
-    localStorage.setItem("theme", "light");
-  }
-  let color = localStorage.getItem("color");
-  if (color) {
-    setInitWallpaper(dataTheme.get(), color);
-  } else {
-    handleWallpaper(dataTheme.get());
-  }
-  let pattern = localStorage.getItem("pattern");
-  if (pattern) {
-    if (pattern === "false") {
-      patternView.set(false);
-    } else {
-      patternView.set(true);
-    }
-  } else {
-    localStorage.setItem("pattern", "true");
-  }
-}
-export const dataTheme = observable.box("light");
-export const selectedTheme = observable.box("light");
-checkTheme();
 
 const App: React.FC = () => {
+  const themeStore = useStore(ThemeContext);
+
   return (
-    <ContextProvider>
-      <div className="flex-container" data-theme={dataTheme.get()}>
-        <div className="top-flex-bar"></div>
-        <div className="flex-wrapper">
-          <Snackbar></Snackbar>
-          <Navbar></Navbar>
-          <ChannelsSection></ChannelsSection>
-          <div className="wrapper-side">
-            <MainChat></MainChat>
-            <SideSection></SideSection>
-          </div>
+    <div className="flex-container" data-theme={themeStore.dataTheme}>
+      <div className="top-flex-bar"></div>
+      <div className="flex-wrapper">
+        <Snackbar></Snackbar>
+        <Navbar></Navbar>
+        <ChannelsSection></ChannelsSection>
+        <div className="wrapper-side">
+          <MainChat></MainChat>
+          <SideSection></SideSection>
         </div>
       </div>
-    </ContextProvider>
+    </div>
   );
 };
 
