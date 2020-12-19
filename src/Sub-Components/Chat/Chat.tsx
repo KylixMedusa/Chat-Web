4567e52fe4d14d7b2a2bc43dc35cd81904fddb2e
KylixMedusa <ayush.agarwalmng@gmail.com>
2020-12-19 22:49:30 +0530

    Added checkbox and tsx for checkbox handling

--- src/Sub-Components/Chat/Chat.tsx
+++ src/Sub-Components/Chat/Chat.tsx
@@ -1,11 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./Chat.scss";
 
 var parse = require("html-react-parser");
 
 type Props = {
-  header?: string|null;
-  focus:boolean;
+  header?: string | null;
+  focus: boolean;
   text: string;
   author: number;
   time: string;
@@ -18,6 +18,7 @@ type Props = {
 const Chat: React.FC<Props> = (props) => {
   const buttonLeft = useRef<HTMLDivElement>(null);
   const buttonRight = useRef<HTMLDivElement>(null);
+  const [checkedState, setCheckedState] = useState(false);
 
   const openList = (val: string) => {
     if (val === "left") {
@@ -66,67 +67,89 @@ const Chat: React.FC<Props> = (props) => {
           <span>{props.header}</span>
         </div>
       ) : null}
-      {props.dir === "left" ? (
-        <div className={`message-left ${props.focus ? "focus" : ""}`}>
-          <span className="message-text">
-            {parse(props.text)}
-            <span className="message-time">{props.time}</span>
-          </span>
-          <span className={`icon-menu ${props.menuClass ? "active" : ""}`}>
-            <span
-              role="button"
-              className={`icon`}
-              onClick={() => {
-                openList("left");
-              }}
-              ref={buttonLeft}
+      <div className="message">
+        <div className="checkbox-card">
+          <div className="checkbox-wrapper">
+            <input
+              className="input-checkbox"
+              type="checkbox"
+              id="desktop-alerts"
+              onChange={() => setCheckedState(!checkedState)}
+              checked={checkedState}
+            />
+            <div
+              className="custom-checkbox-wrapper"
+              onClick={() => setCheckedState(!checkedState)}
             >
-              <i>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"
-                  ></path>
-                </svg>
-              </i>
-            </span>
-          </span>
+              <div className="custom-checkbox">
+                <div className="tick-mark"></div>
+              </div>
+            </div>
+          </div>
         </div>
-      ) : (
-        <div className={`message-right ${props.focus ? "focus" : ""}`}>
-          <span className={`icon-menu ${props.menuClass ? "active" : ""}`}>
-            <span
-              role="button"
-              className={`icon`}
-              onClick={() => openList("right")}
-              ref={buttonRight}
-            >
-              <i>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"
-                  ></path>
-                </svg>
-              </i>
+
+        {props.dir === "left" ? (
+          <div className={`message-left ${props.focus ? "focus" : ""}`}>
+            <span className="message-text">
+              {parse(props.text)}
+              <span className="message-time">{props.time}</span>
             </span>
-          </span>
-          <span className="message-text">
-            {parse(props.text)}
-            <span className="message-time">{props.time}</span>
-          </span>
-        </div>
-      )}
+            <span className={`icon-menu ${props.menuClass ? "active" : ""}`}>
+              <span
+                role="button"
+                className={`icon`}
+                onClick={() => {
+                  openList("left");
+                }}
+                ref={buttonLeft}
+              >
+                <i>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"
+                    ></path>
+                  </svg>
+                </i>
+              </span>
+            </span>
+          </div>
+        ) : (
+          <div className={`message-right ${props.focus ? "focus" : ""}`}>
+            <span className={`icon-menu ${props.menuClass ? "active" : ""}`}>
+              <span
+                role="button"
+                className={`icon`}
+                onClick={() => openList("right")}
+                ref={buttonRight}
+              >
+                <i>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M12 7a2 2 0 1 0-.001-4.001A2 2 0 0 0 12 7zm0 2a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 9zm0 6a2 2 0 1 0-.001 3.999A2 2 0 0 0 12 15z"
+                    ></path>
+                  </svg>
+                </i>
+              </span>
+            </span>
+            <span className="message-text">
+              {parse(props.text)}
+              <span className="message-time">{props.time}</span>
+            </span>
+          </div>
+        )}
+      </div>
     </React.Fragment>
   );
 };
